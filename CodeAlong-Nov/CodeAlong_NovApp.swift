f6a5e88fdc73aec009c9bd429c8b605c1eb13350
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2025-11-15 02:47:39 +0700

    Initial Commit

---
+++ CodeAlong-Nov/CodeAlong_NovApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeAlong_NovApp.swift
+//  CodeAlong-Nov
+//
+//  Created by putragandadewata on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CodeAlong_NovApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeAlong-Nov/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeAlong-Nov
+//
+//  Created by putragandadewata on 15/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
